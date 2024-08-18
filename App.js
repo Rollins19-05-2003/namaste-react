29996cc001a64c718eaf305082d2e6e2a9cfcbf0
Rollins19-05-2003 <sdeb7433@gmail.com>
2024-08-18 13:04:36 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,26 @@
+// const heading = React.createElement("h1", {id:"heading"}, "Hello world from React !!");
+// const root = ReactDOM.createRoot(document.getElementById("header"));
+// root.render(heading);
+
+
+
+const parent = React.createElement("div", {id:"parent"},
+    [
+        React.createElement("div", {id:"child1"},
+            [
+                React.createElement("h1", {id:"heading"}, "I am h1 tag"),
+                React.createElement("h2", {id:"heading"}, "I am h2 tag")
+            ]
+        ),
+    
+        React.createElement("div", {id:"child2"},
+            [
+                React.createElement("h1", {id:"heading"}, "I am h1 tag"),
+                React.createElement("h2", {id:"heading"}, "I am h2 tag")
+            ]
+        ),
+    ]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("header"));
+root.render(parent);
